41cd0f7af07dd994f4d50085b582c2b2c1bc2ac5
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2021-12-08 05:58:11 +0400

    Extension

---
+++ Occurance/Occurance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Occurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello welcome to welcome my first Hello Welcome Console Application";
+            string searchItem = "welcome";
+            int count = new Regex(searchItem, RegexOptions.Compiled).Matches(str).Count;
+            Console.WriteLine("{0} Matches found.", count);
+        }
+    }
+}
